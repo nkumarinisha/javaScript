427eb69f583740f3feacf81f5fa7e5691a76fe06
Nisha kumari <137318555+nkumarinisha@users.noreply.github.com>
2024-12-11 08:23:49 +0000

    datatype done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,58 @@
+/*
+let score=65
+let valstr=String(score);
+console.log(valstr);
+
+console.log(typeof valstr);
+
+let sc="jaishree123"
+console.log(sc);//jaishree123
+let consc= Number(sc);
+console.log(typeof consc);//number
+console.log(consc);//NaN
+
+let sc=null
+console.log(sc);
+//let consc= Number(sc);
+console.log(typeof sc);//object
+//console.log(consc);
+
+let sc=undefined
+console.log(sc);
+let consc= Number(sc);
+console.log(typeof sc);
+console.log(consc);
+*/
+let sc=true
+// console.log(sc);
+// let consc= Number(sc);
+// console.log(typeof sc);
+// console.log(consc);
+
+let a=6 , b=4;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a%b);
+let str1="Nitya"
+let str2="Rani"
+let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2+3);
+// console.log(1+21+"2");
+// console.log(undefined+12);
+// console.log(null+12);
+// console.log("12"+null);
+// console.log(true+3);
+
+//Q1.create a variable of type string and try to add a numbers to it.
+let str ="nisha";
+let num=62
+let res=str+num
+console.log(res);
+console.log(typeof res);
+
+
+
