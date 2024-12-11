411036401911296b1f62511de3d88774556f788e
Nisha kumari <137318555+nkumarinisha@users.noreply.github.com>
2024-12-11 06:42:15 +0000

    container done

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const acc_id=12
+let acc_email="nisha12@gmail.com"
+var acc_pass="1234"
+acc_city="Bengalore"
+
+//acc_id=6 //it is not allowed because const variable is neither reinitialized nor redeclared.
+acc_email="nk12@gmail.com"
+acc_pass="444"
+acc_city="bokaro"
+console.table([acc_id,acc_email,acc_pass,acc_city]);// it returns the o/p in tabular form
+/* Most prefer not to use var b/c of the issue in block scope and function scope  */21
